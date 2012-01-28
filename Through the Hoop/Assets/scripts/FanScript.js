1c64f94b9f959661f9d3ad40fa28a580f20fe918
David.To.FS@gmail.com <David.To.FS@gmail.com@dd5142e0-20a9-022a-7118-1aa6c53efe0e>
2012-01-28 18:21:30 +0000

    fan script fixed

--- Through the Hoop/Assets/scripts/FanScript.js
+++ Through the Hoop/Assets/scripts/FanScript.js
@@ -1,48 +1,31 @@
-var ballTransform:Transform;
-var ball:GameObject;
-var inTrigger:boolean;
+private var ball:GameObject;
+
 
 
 function start(){
-    inTrigger = false;
+    
 }
 
-function FixedUpdate(){
-
-    if(inTrigger){
-        var myTransform:Transform = gameObject.GetComponent("Transform");
+function OnTriggerStay (other : Collider) {
+    ball = other.gameObject;
+    ballTransform = other.transform;
+    
+    var myTransform:Transform = gameObject.GetComponent("Transform");
  		////Debug.Log("My fan position is "+ballTransform.position);
     	//Debug.Log("My position is "+myTransform.position);
     
     	//compare relative positions and angle between ball position and fan force
-    	var ballDirection =  ballTransform.position - myTransform.position;
+    	//var ballDirection =  ballTransform.position - myTransform.position;
+    	var ballDirection = -myTransform.up;
     	ballDirection = Vector3(ballDirection.x,ballDirection.y,-2);
 		//var AngleBetween = Vector3.Angle(Vector3.up, ballDirection);
 		//Debug.Log(ballDirection + " , " + AngleBetween);
 	
 		var ballDistance = Vector3.Distance(myTransform.position,ballTransform.position);
 		//Debug.Log(ballDistance);
-		if( ballDistance < 5.0 ){
+	
 			ball.GetComponent("Rigidbody").AddForce(ballDirection * ( 16.5 - ballDistance));
-			//Debug.Log("In Wind");
-		}       
- 
-    }
-}
-
-function OnTriggerEnter (other : Collider) {
-
-    inTrigger = true;
-    Debug.Log("in trigger");
-    
-    ball = other.gameObject;
-    ballTransform = other.transform;
-}
-
-function onTriggerExit(other : Collider){
-	inTrigger = false;
 	
-	Debug.Log("out of trigger");
-
-	
+	Debug.Log("is inside trigger");
 }
+
