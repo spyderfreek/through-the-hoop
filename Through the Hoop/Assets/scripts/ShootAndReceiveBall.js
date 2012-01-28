1141b0c9124453a6214b24fb12e9c7e67f4e7dc3
David.To.FS@gmail.com <David.To.FS@gmail.com@dd5142e0-20a9-022a-7118-1aa6c53efe0e>
2012-01-28 20:30:20 +0000

    Screen will now fade when the ball go though the first time. when fading it done it will go to the WinScreen. Note. make sure you include the WinScreen.unity in the build settings

--- Through the Hoop/Assets/scripts/ShootAndReceiveBall.js
+++ Through the Hoop/Assets/scripts/ShootAndReceiveBall.js
@@ -7,30 +7,80 @@ private var fTimePassed:float = 0.0f;
 public var fTimeToCheck:float = 1.0f;
 
 //keep an instinse of a ball
-private var goBall:GameObject;
+private var goBall:GameObject = null;
 
 //keep track of how long will the win screen will last
+public var fWinTimeScale:float = 1.5f;
 
-function Update () {
-if (Input.GetKeyDown ("space"))
-{
- ShootBall();
+private var fTimeSinceFirstLoop = 0.0f;
+public var fTimeWinDiration = 5.0f;
+
+
+
+private var bWin = false;
+var blackTexture : Texture;
+
+public var szWinScene;
+
+function OnGUI(){
+
+//using whatever texture is fine as long as there is no transparent textures
+
+
+ 	GUI.color = new Color(0, 0, 0, fTimeSinceFirstLoop/ fTimeWinDiration);
+	GUI.DrawTexture( new Rect(0, 0, Screen.width, Screen.height ), blackTexture );
 }
 
-if(bBallLaunched == true)
-{
+function Update () {
 
-	fTimePassed += Time.deltaTime;
-	if(fTimePassed > fTimeToCheck)
+
+	if (Input.GetKeyDown ("space"))
 	{
-	fTimePassed = fTimeToCheck;
-	Physics.IgnoreCollision(goBall.collider, collider,false);
+ 	ShootBall();
+	}
+
+	if(bBallLaunched == true)
+	{
+
+		fTimePassed += Time.deltaTime;
+		if(fTimePassed > fTimeToCheck)
+		{
+		fTimePassed = fTimeToCheck;
+		//if a cirtain amount has passed since the ball been launch set the collider back on.
+		Physics.IgnoreCollision(goBall.collider, collider,false);
+		}
 	
 	}
 	
+	if(bWin == true)
+	{
+	fTimeSinceFirstLoop += Time.deltaTime / Time.timeScale;
+	
+		if(fTimeSinceFirstLoop > fTimeWinDiration)
+		{
+			//change to win scene
+			Application.LoadLevel("WinScreen");
+		}
+	
+	}
+	
+
+
 }
 
-
+//Reset everything
+function Init()
+{
+	bBallLaunched = false;
+	if(goBall != null)
+	{
+	Destroy(goBall);
+	}
+	fTimePassed = 0.0f;
+	fTimeSinceFirstLoop = 0.0f;
+	
+	bWin = false;
+	
 }
 
 function ShootBall()
@@ -43,9 +93,13 @@ function ShootBall()
 	
 	bBallLaunched = true;
 	goBall = Instantiate(prefab, transform.position, Quaternion.identity);
-
+	fTimeSinceFirstLoop = 0.0f;
+	
 	//make sure that collison form fire point is off.
 	Physics.IgnoreCollision(goBall.collider, collider);
+	
+	bWin = false;
+	
 
 }
 
@@ -55,10 +109,13 @@ function WinHit()
 {
 
 Debug.Log("Winhit");
+	bWin = true;
 goBall.transform.position = this.transform.position;
 goBall.rigidbody.velocity.x = 0.0;
 goBall.rigidbody.velocity.y = 0.0;
 
+
+Time.timeScale = fWinTimeScale;
 
 }
 
