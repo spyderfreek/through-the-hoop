ecc8e98b01653bbff8925708d38a422a1cefa7da
nkitten@gmail.com <nkitten@gmail.com@dd5142e0-20a9-022a-7118-1aa6c53efe0e>
2012-01-28 10:35:48 +0000

    fixed mouseover script for multi-component meshes.

--- trunk/Through the Hoop/Assets/scripts/Environment_MouseOver.js
+++ trunk/Through the Hoop/Assets/scripts/Environment_MouseOver.js
@@ -1,16 +1,31 @@
 var mat:Material;
+var hasMultipleObjects:boolean = false;
 var glowColor:Color = Color(1,1,1,0.5);
 var normalColor:Color = Color(1,1,1,0);
 
 
 function OnMouseEnter() 
 {
-	mat.SetColor("_Color", glowColor);
+	if(hasMultipleObjects) {
+		for( var child : Transform in transform.parent ) {
+			child.gameObject.GetComponent(Renderer).material.SetColor("_Color", glowColor);
+		}
+	}
+	else {
+		mat.SetColor("_Color", glowColor);
+	}
 	Debug.Log("mouse enter");
 }
 
 function OnMouseExit()
 {	
-	mat.SetColor("_Color", normalColor);
+	if(hasMultipleObjects) {
+		for( var child : Transform in transform.parent ) {
+			child.gameObject.GetComponent(Renderer).material.SetColor("_Color", normalColor);
+		}
+	}
+	else {
+		mat.SetColor("_Color", normalColor);
+	}
 	Debug.Log("mouse exit");
 }
