c5609cc147e6f4be7e96d1859d798164ddb0c81b
nkitten@gmail.com <nkitten@gmail.com@dd5142e0-20a9-022a-7118-1aa6c53efe0e>
2012-01-29 01:03:28 +0000

    fixed user object positioning.

--- Through the Hoop/Assets/scripts/Environment_MouseOver.js
+++ Through the Hoop/Assets/scripts/Environment_MouseOver.js
@@ -3,9 +3,47 @@ var hasMultipleObjects:boolean = false;
 var glowColor:Color = Color(1,1,1,0.5);
 var normalColor:Color = Color(1,1,1,0);
 
+private var selectedObject: GameObject;
+private var haveSelected: boolean;
+
+private var leftUpper:Vector3;
+private var rightLower:Vector3;
+
+private var xratio: double;
+private var yratio: double;
+
+private var ballPlane: Plane;
+private var offsetVec: Vector3;
+
 function Start()
 {
 	mat.SetColor("_Color", normalColor);
+	
+	//get scene coordinates for reference
+	var ray : Ray = Camera.main.ScreenPointToRay(Vector3(0,0,0)); 
+	var hit : RaycastHit;
+	 if (Physics.Raycast(ray, hit)) {
+	 Debug.Log("first hit");
+	      leftUpper = hit.point;
+	 }
+	 ray = Camera.main.ScreenPointToRay(Vector3(Screen.width,Screen.height,0)); 
+	 if (Physics.Raycast(ray, hit)) {
+	      rightLower = hit.point;
+	       Debug.Log("second hit");
+	 }
+	 Debug.Log(leftUpper.y);
+	 Debug.Log(rightLower.y);
+	 
+	 xratio = ( leftUpper.x - rightLower.x )/Screen.width;
+	 Debug.Log(xratio);
+	 yratio = ( rightLower.y - leftUpper.y )/Screen.height;
+	 Debug.Log(yratio);
+	 
+	 ballPlane = Plane( Vector3(0,0,1), GameObject.Find("BallStart").GetComponent(Transform).position );
+	 
+	 haveSelected = false;
+	 selectedObject = null;
+
 }
 
 function OnMouseEnter() 
@@ -19,7 +57,7 @@ function OnMouseEnter()
 		}
 	}
 	else {
-		mat.SetColor("_Color", glowColor);
+		GetComponent(Renderer).material.SetColor("_Color", glowColor);
 	}
 	Debug.Log("mouse enter");
 }
@@ -35,7 +73,59 @@ function OnMouseExit()
 		}
 	}
 	else {
-		mat.SetColor("_Color", normalColor);
+		GetComponent(Renderer).material.SetColor("_Color", normalColor);
 	}
 	Debug.Log("mouse exit");
+}
+
+function OnMouseDown()
+{
+	//mouse click controls
+	if(Input.GetMouseButtonDown(0)){
+	    var hit : RaycastHit;
+        //find where the user clicked on the screen
+        var ray : Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        //if an object was hit, drag object 
+
+	    haveSelected = true;    
+	    var dist:float;
+	    ballPlane.Raycast(ray, dist);
+	    
+	    var objectPos:Vector3 = hasMultipleObjects ? transform.parent.position : transform.position;
+	    offsetVec = objectPos - ray.direction * dist;
+	}
+}
+
+function OnMouseDrag()
+{	
+	if (Input.GetMouseButton(0)) {
+   
+           //var OldPosition = selectedObject.GetComponent("Transform").position;
+          // Debug.Log("Old position is "+OldPosition);
+          // Debug.Log("New x is "+(OldPosition.x + Input.mousePosition.x*xratio));
+           if(haveSelected) {
+           		//find where the user clicked on the screen
+        		var ray : Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        		var dist: float;
+        		ballPlane.Raycast(ray, dist);
+        		
+        		if( hasMultipleObjects ) {
+        			transform.parent.position = ray.direction * dist + offsetVec;
+        		}
+        		else {
+        			transform.position = ray.direction * dist + offsetVec;
+        		}
+           }
+
+    }
+    
+}
+
+function OnMouseUp()
+{
+    if(Input.GetMouseButtonUp(0)){
+        Debug.Log("I am stupid");
+        if(haveSelected) haveSelected = false;
+    
+    }
 }
