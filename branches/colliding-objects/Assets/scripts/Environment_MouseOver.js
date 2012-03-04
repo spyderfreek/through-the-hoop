42d3cf1ca4e2494fc1163c0cdffe1a7caaa15689
nkitten@gmail.com <nkitten@gmail.com@dd5142e0-20a9-022a-7118-1aa6c53efe0e>
2012-03-04 01:21:50 +0000

    committing intermediate changes for object positioning upgrade.

--- branches/colliding-objects/Assets/scripts/Environment_MouseOver.js
+++ branches/colliding-objects/Assets/scripts/Environment_MouseOver.js
@@ -15,6 +15,19 @@ private var yratio: double;
 private var ballPlane: Plane;
 private var offsetVec: Vector3;
 
+private var moveForceProportional: double = 6;
+// since there's no steady-state error to contend with (i.e. fighting against gravity)
+// we dont' really need an integral term, but I'm leaving this in for completeness.
+private var moveForceIntegral: double = 0;
+private var moveForceDifferential: double = 4;
+
+private var moveError: Vector3 = Vector3.zero;
+private var moveErrorIntegral: Vector3 = Vector3.zero;
+private var moveErrorDifferential: Vector3 = Vector3.zero;
+
+private var constraints: RigidbodyConstraints;
+
+
 function GetLaunched()
 {
 	return GameObject.Find("BallStart").GetComponent("ShootAndReceiveBall").bBallLaunched;
@@ -23,6 +36,11 @@ function GetLaunched()
 function Start()
 {
 	mat.SetColor("_Color", normalColor);
+	
+	if( rigidbody ) {
+		constraints = rigidbody.constraints;
+		Debug.Log("Down: " + Vector3.down);
+	}
 	
 	//get scene coordinates for reference
 	var ray : Ray = Camera.main.ScreenPointToRay(Vector3(0,0,0)); 
@@ -67,7 +85,7 @@ function OnMouseEnter()
 	else {
 		GetComponent(Renderer).material.SetColor("_Color", glowColor);
 	}
-	Debug.Log("mouse enter");
+	//Debug.Log("mouse enter");
 }
 
 function OnMouseExit()
@@ -83,7 +101,7 @@ function OnMouseExit()
 	else {
 		GetComponent(Renderer).material.SetColor("_Color", normalColor);
 	}
-	Debug.Log("mouse exit");
+	//Debug.Log("mouse exit");
 }
 
 function OnMouseDown()
@@ -100,11 +118,19 @@ function OnMouseDown()
         //if an object was hit, drag object 
 
 	    haveSelected = true;    
+	    rigidbody.isKinematic = false;
+	    rigidbody.constraints = constraints;
+	    //rigidbody.useGravity = false;
+	    
 	    var dist:float;
 	    ballPlane.Raycast(ray, dist);
 	    
 	    var objectPos:Vector3 = hasMultipleObjects ? transform.parent.position : transform.position;
 	    offsetVec = objectPos - ray.direction * dist;
+	    
+	    moveError = Vector3.zero;
+	    moveErrorIntegral = Vector3.zero;
+	    moveErrorDifferential = Vector3.zero;
 	}
 }
 
@@ -119,6 +145,7 @@ function OnMouseDrag()
            //var OldPosition = selectedObject.GetComponent("Transform").position;
           // Debug.Log("Old position is "+OldPosition);
           // Debug.Log("New x is "+(OldPosition.x + Input.mousePosition.x*xratio));
+          /*
            if(haveSelected) {
            		//find where the user clicked on the screen
         		var ray : Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -126,16 +153,55 @@ function OnMouseDrag()
         		ballPlane.Raycast(ray, dist);
         		
         		if( hasMultipleObjects ) {
-        			transform.parent.position = ray.direction * dist + offsetVec;
+        			//transform.parent.position = ray.direction * dist + offsetVec;
+        			transform.parent.rigidbody.AddForce( moveForceProportional * (ray.direction * dist - transform.parent.position + offsetVec) );
         		}
         		else {
-        			transform.position = ray.direction * dist + offsetVec;
+        			//transform.position = ray.direction * dist + offsetVec;
+        			var newMoveError = ray.direction * dist - transform.position + offsetVec;
+        			Debug.Log("Error = " + newMoveError.magnitude);
+        			moveErrorIntegral += newMoveError * Time.deltaTime;
+        			moveErrorDifferential = (newMoveError - moveError)/Time.deltaTime;
+        			rigidbody.AddForce( moveForceProportional * newMoveError + moveForceIntegral * moveErrorIntegral + moveForceDifferential * moveErrorDifferential );
+        		}
+           }
+			*/
+    }
+    
+}
+
+function FixedUpdate() {
+	if (Input.GetMouseButton(0)) {
+   
+           //var OldPosition = selectedObject.GetComponent("Transform").position;
+          // Debug.Log("Old position is "+OldPosition);
+          // Debug.Log("New x is "+(OldPosition.x + Input.mousePosition.x*xratio));
+           if(haveSelected) {
+           		//find where the user clicked on the screen
+        		var ray : Ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        		var dist: float;
+        		ballPlane.Raycast(ray, dist);
+        		
+        		if( hasMultipleObjects ) {
+        			//transform.parent.position = ray.direction * dist + offsetVec;
+        			transform.parent.rigidbody.AddForce( moveForceProportional * (ray.direction * dist - transform.parent.position + offsetVec) );
+        		}
+        		else {
+        			//transform.position = ray.direction * dist + offsetVec;
+        			var newMoveError = ray.direction * dist - transform.position + offsetVec;
+        			//Debug.Log("Error = " + newMoveError.magnitude);
+        			moveErrorIntegral += newMoveError * Time.deltaTime;
+        			moveErrorDifferential = (newMoveError - moveError)/Time.deltaTime;
+        			rigidbody.AddForce( moveForceProportional * newMoveError + moveForceIntegral * moveErrorIntegral + moveForceDifferential * moveErrorDifferential );
+        			//rigidbody.MovePosition(newMoveError);
+        			moveError = newMoveError;
         		}
            }
 
     }
     
 }
+
 
 function OnMouseUp()
 {
@@ -145,6 +211,24 @@ function OnMouseUp()
 	
     if(Input.GetMouseButtonUp(0)){
         haveSelected = false;
-    
+        //rigidbody.velocity = Vector3.zero;
+        //rigidbody.useGravity = true;
+        
+        var hitInfo:RaycastHit;
+        
+        Debug.DrawLine( transform.position, transform.position  + Vector3.down * 5, Color.red, 1 );
+        
+        if( rigidbody.SweepTest(Vector3.down, hitInfo, 5) ) {
+        	rigidbody.isKinematic = true;
+        	transform.position += hitInfo.distance * Vector3.down;
+	        Debug.Log( "test hit: " + hitInfo.distance );
+        	//transform.position += hitInfo.distance * Vector3.down;
+        }
+        else {
+        	Debug.Log( "Sweep test failed!" );
+        }
+        //rigidbody.constraints = constraints | RigidbodyConstraints.FreezePositionX;
+        
+        rigidbody.isKinematic = true;
     }
 }
